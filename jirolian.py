e087af3ac52b6cfc6d67399b897b2848bdde81a7
sishikawa <18254930+ShuIshikawa@users.noreply.github.com>
2023-01-08 17:26:25 +0900

    Update jirolian.py

--- jirolian.py
+++ jirolian.py
@@ -13,9 +13,36 @@ ACCESS_TOKEN_SECRET = ''
 USER_ID = 0
 
 
-def logging(text):
-    with open(__file__+'.log', 'a') as f:
-        print(text, file=f)
+def bearer_oauth_following(r):
+    r.headers['Authorization'] = f'Bearer {BEARER_TOKEN}'
+    r.headers['User-Agent'] = 'v2FollowingLookupPython'
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.request(
+        'GET',
+        url,
+        auth=bearer_oauth_following,
+        params=params
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            'Request returned an error: {} {}'.format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def get_following_usernames():
+    url = f'https://api.twitter.com/2/users/{USER_ID}/following'
+    params = {'user.fields': 'created_at'}
+    following_usernames = {
+        user['username'] for user in connect_to_endpoint(url, params)['data']
+    }
+    return following_usernames
 
 
 def create_retweet(tweet_id):
@@ -26,7 +53,6 @@ def create_retweet(tweet_id):
         resource_owner_secret=ACCESS_TOKEN_SECRET,
     )
 
-    # Making the request
     response = oauth.post(
         f'https://api.twitter.com/2/users/{USER_ID}/retweets',
         json={'tweet_id': tweet_id}
@@ -38,11 +64,7 @@ def create_retweet(tweet_id):
         )
 
 
-def bearer_oauth(r):
-    '''
-    Method required by bearer token authentication.
-    '''
-
+def bearer_oauth_filtered_stream(r):
     r.headers['Authorization'] = f'Bearer {BEARER_TOKEN}'
     r.headers['User-Agent'] = 'v2FilteredStreamPython'
     return r
@@ -50,13 +72,14 @@ def bearer_oauth(r):
 
 def get_rules():
     response = requests.get(
-        'https://api.twitter.com/2/tweets/search/stream/rules', auth=bearer_oauth
+        'https://api.twitter.com/2/tweets/search/stream/rules',
+        auth=bearer_oauth_filtered_stream,
     )
     if response.status_code != 200:
         raise Exception(
             'Cannot get rules (HTTP {}): {}'.format(response.status_code, response.text)
         )
-    logging(json.dumps(response.json()))
+    print(json.dumps(response.json()))
     return response.json()
 
 
@@ -68,7 +91,7 @@ def delete_all_rules(rules):
     payload = {'delete': {'ids': ids}}
     response = requests.post(
         'https://api.twitter.com/2/tweets/search/stream/rules',
-        auth=bearer_oauth,
+        auth=bearer_oauth_filtered_stream,
         json=payload
     )
     if response.status_code != 200:
@@ -77,32 +100,33 @@ def delete_all_rules(rules):
                 response.status_code, response.text
             )
         )
-    logging(json.dumps(response.json()))
+    print(json.dumps(response.json()))
 
 
 def set_rules():
-    # You can adjust the rules if needed
-    sample_rules = [
+    new_rules = [
         {'value': '-is:retweet @jirolian', 'tag': 'at jiolian'},
     ]
-    payload = {'add': sample_rules}
+    payload = {'add': new_rules}
     response = requests.post(
         'https://api.twitter.com/2/tweets/search/stream/rules',
-        auth=bearer_oauth,
+        auth=bearer_oauth_filtered_stream,
         json=payload,
     )
     if response.status_code != 201:
         raise Exception(
             'Cannot add rules (HTTP {}): {}'.format(response.status_code, response.text)
         )
-    logging(json.dumps(response.json()))
+    print(json.dumps(response.json()))
 
 
-def get_stream():
+def get_stream(usernames):
     response = requests.get(
-        'https://api.twitter.com/2/tweets/search/stream?tweet.fields=entities', auth=bearer_oauth, stream=True,
+        'https://api.twitter.com/2/tweets/search/stream?tweet.fields=entities',
+        auth=bearer_oauth_filtered_stream,
+        stream=True,
     )
-    logging(response.status_code)
+    print(response.status_code)
     if response.status_code != 200:
         raise Exception(
             'Cannot get stream (HTTP {}): {}'.format(
@@ -112,31 +136,39 @@ def get_stream():
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
-            logging(json_response)
-            if 'entities' in json_response['data']:
-                if 'mentions' in json_response['data']['entities']:
-                    if len(json_response['data']['entities']['mentions'])==1:
-                        if json_response['data']['entities']['mentions'][0]['username']=='jirolian':
-                            create_retweet(json_response['data']['id'])
-                    elif 'urls' in json_response['data']['entities']:
-                        if any([('www.swarmapp.com' in url.get('expanded_url', '')) for url in json_response['data']['entities']['urls']]):
+            print(datetime.datetime.now())
+            print(json_response)
+            if 'errors' in json_response:
+                time.sleep(60)
+                continue
+            elif 'data' in json_response:
+                if 'entities' in json_response['data']:
+                    if 'mentions' in json_response['data']['entities']:
+                        mentions = set(map(
+                            lambda x: x['username'],
+                            json_response['data']['entities']['mentions']
+                        ))
+                        if len(mentions-usernames) == 0:
                             create_retweet(json_response['data']['id'])
 
 
 def main():
+    usernames = get_following_usernames()
+    print('Following Usernames')
+    print(usernames)
     rules = get_rules()
     delete_all_rules(rules)
     set_rules()
     while True:
         try:
-            get_stream()
+            get_stream(usernames)
         except Exception as e:
-            logging(datetime.datetime.now())
-            logging(e)
+            print(datetime.datetime.now())
+            print(e)
         time.sleep(60)
 
 
 if __name__ == '__main__':
     main()
 
-# nohup python jirolian.py > jirolian.log &
+# nohup python jirolian.py >> jirolian.log &
